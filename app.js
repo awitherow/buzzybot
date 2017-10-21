cc6a7b287a8241bd916fdca61c5e9e7aae5a2e48
Jack Coble <jackcoble02@gmail.com>
2017-10-21 21:53:57 +0100

    Added search params for Buzzcoin and mark as favourite

--- app.js
+++ app.js
@@ -1,11 +1,34 @@
 var Twitter = require('twitter');
 var client = new Twitter(require('./config.js'));
 
-client.post('statuses/update', {status: 'Wooooo! I got the BuzzyBot running!'}, function(error, tweet, response) {
-  if (!error) {
-    console.log(tweet);
+console.log("Hooray! Buzzy Bot is running!");
+
+//Search parameters
+var params = {
+  q: '#buzzcoin',
+  count: 10,
+  result_type: 'recent',
+  lang: 'en'
+}
+
+//Start a search for Buzzcoin related tweets
+client.get('search/tweets', params, function(err, data, response) {
+  if(!err){
+    for(let i = 0; i < data.statuses.length; i++){
+      // Get the tweet Id from the returned data
+      let id = { id: data.statuses[i].id_str }
+      client.post('favorites/create', id, function(err, response){
+        if(err){
+          console.log(err[0].message);
+        }
+        else{
+          let username = response.user.screen_name;
+          let tweetId = response.id_str;
+          console.log('Favorited: ', `https://twitter.com/${username}/status/${tweetId}`)
+        }
+      });
+    }
+  } else {
+    console.log(err);
   }
-  else {
-    console.log(error);
-  }
-});
+})
