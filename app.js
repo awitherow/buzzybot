61620587068004ab31186e6f4e08375274c86691
Jack Coble <jackcoble02@gmail.com>
2017-10-22 11:46:30 +0100

    Using twit module and defined search params

--- app.js
+++ app.js
@@ -1,3 +1,20 @@
 const twit = require('twit');
 const config = require('./config');
 const client = new twit(config);
+
+require('dotenv');
+
+function searchTwitter() {
+  const params = {
+    q: '#buzzcoin',
+    count: 100,
+    result_type: ['recent', 'popular'],
+    language: 'en'
+  }
+
+  return client.get('search/tweets', params)
+    .then(function(res) {
+      console.log(res);
+    });
+}
+searchTwitter();
