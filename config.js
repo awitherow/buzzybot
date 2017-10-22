fdbff4e035f5bafc867171fc5a19f1bbd4754c8a
Jack Coble <jackcoble02@gmail.com>
2017-10-22 11:26:18 +0100

    Using .env to store API credentials

--- config.js
+++ config.js
@@ -1,6 +1,4 @@
+require('dotenv').load();
 module.exports = {
-  consumer_key: 'YOUR_CONSUMER_KEY',
-  consumer_secret: 'YOUR_CONSUMER_SECRET',
-  access_token: 'YOUR_ACCESS_TOKEN',
-  access_token_secret: 'YOUR_ACCESS_TOKEN_SECRET'
-};
+  
+}
