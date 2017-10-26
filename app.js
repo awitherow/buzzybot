7620eda24bf8b4dcc8194a1fed6fa90b7009c889
awitherow <au.witherow@gmail.com>
2017-10-27 00:02:23 +0200

    add new tweets

--- app.js
+++ app.js
@@ -50,7 +50,7 @@ var categories = [
     name: "os",
     tweets: [
       "#Passionate for #blockchain and #development? Get $BUZZ with our #opensource incentive program! https://goo.gl/1vCMss #innovation #buzzcoin #buzzfam",
-      "OS_SECOND_TWEET"
+      "At $BUZZ, our entire infrastructure is #opensource, and for all work done, we reward in $BUZZ. Get Involved Today at https://goo.gl/1vCMss"
     ],
     active: true
   },
@@ -58,15 +58,15 @@ var categories = [
     name: "community",
     tweets: [
       "Get 24/7 $BUZZ support by joining our Discord server! https://t.co/XghGz66wYo #community #blockchain #cryptocommunity",
-      "COMM_SECOND_TWEET"
+      "Like @telegram? $BUZZ got your back! Join us there, today! https://t.me/joinchat/EqD7e0FNSbRzjGwBRiv5cQ #community #blockchain #buzzfam"
     ],
     active: true
   },
   {
     name: "get-started",
     tweets: [
-      "New to $BUZZ? Getting started has never been easier at http://buzzcoin.info/get-started.html",
-      "GET_STARTED_SECOND_TWEET"
+      "New to $BUZZ? Getting started has never been easier at http://buzzcoin.info/get-started.html $BUZZ #buzzcoin #community",
+      "Wanna join the #BUZZFAM but don't know how to get set up? We got a guide for that! Check it! http://buzzcoin.info/get-started.html #buzzcoin $BUZZ #community"
     ],
     active: true
   },
@@ -74,7 +74,7 @@ var categories = [
     name: "pool",
     tweets: [
       "Interesting in getting #ProofofStake #rewards daily with $BUZZ? Join the #official #buzz #pool today! buzzcoin.info",
-      "POOL_SECOND_TWEET"
+      "Don't have a lot of $BUZZ? Waiting forever to stake? Earn guaranteed daily stake rewards with the #official $buzz #pool -> buzzcoin.info"
     ],
     active: false
   }
